5217e2d9dd2e421782873f307fb11d43926e206d
Alexey <cappuccino13371@gmail.com>
2024-02-03 18:41:57 +0300

    Final_1 update

--- main.py
+++ main.py
@@ -2,3 +2,43 @@ from menu import Menu
 from pet_registry import PetRegistry
 from Counter.counter import Counter
 from datetime import datetime
+
+def main():
+    registry = PetRegistry()
+
+    while True:
+        Menu.display_menu()
+        choice = Menu.get_user_choice()
+
+        if choice == "1":
+            pet_type = int(input("Введите тип животного (1 - Cat, 2 - Dog, 3 - Hamster): "))
+            name = input("Введите имя животного: ")
+            birthday = datetime.now()  # Можно изменить на ввод реальной даты
+            registry.add_pet(pet_type, name, birthday)
+            print("Животное добавлено в реестр.")
+
+        elif choice == "2":
+            pet_id = int(input("Введите ID животного для определения класса: "))
+            result = registry.determine_pet_class(pet_id)
+            print(result)
+
+        elif choice == "3":
+            pet_id = int(input("Введите ID животного для просмотра команд: "))
+            result = registry.show_pet_commands(pet_id)
+            print(result)
+
+        elif choice == "4":
+            pet_id = int(input("Введите ID животного для обучения команде: "))
+            command = input("Введите новую команду для обучения: ")
+            result = registry.teach_pet_command(pet_id, command)
+            print(result)
+
+        elif choice == "5":
+            print("Выход из программы.")
+            break
+
+        else:
+            print("Неверный ввод. Пожалуйста, выберите действие от 1 до 5.")
+
+if __name__ == "__main__":
+    main()
--- menu.py
+++ menu.py
@@ -1,2 +1,15 @@
 from Model.pets import PetType, PetCreator
 from Counter.counter import Counter
+
+class Menu:
+    @staticmethod
+    def display_menu():
+        print("1. Завести новое животное")
+        print("2. Определить животное в правильный класс")
+        print("3. Увидеть список команд, которые выполняет животное")
+        print("4. Обучить животное новым командам")
+        print("5. Выйти из программы")
+
+    @staticmethod
+    def get_user_choice():
+        return input("Выберите действие (1-5): ")
